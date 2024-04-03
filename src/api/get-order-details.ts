c87a0d736747e6d6415f5c3c5a5c44546153d9de
Felipe Fardo <felipe.fardo@hotmail.com.br>
2024-04-03 16:44:42 -0300

    order details

---
+++ src/api/get-order-details.ts
@@ -0,0 +1,30 @@
+import { api } from '@/lib/axios'
+
+export interface GetOrdersDetailsParams {
+  orderId: string
+}
+
+export interface GetOrderDetailsResponse {
+  id: string
+  createdAt: string
+  status: 'pending' | 'canceled' | 'processing' | 'delivering' | 'delivered'
+  totalInCents: number
+  customer: {
+    name: string
+    email: string
+    phone: string | null
+  }
+  orderItems: {
+    id: string
+    priceInCents: number
+    quantity: number
+    product: {
+      name: string
+    }
+  }[]
+}
+
+export async function getOrdersDetails({ orderId }: GetOrdersDetailsParams) {
+  const respoonse = await api.get<GetOrderDetailsResponse>(`/orders/${orderId}`)
+  return respoonse.data
+}
